e639ebd1dd4f3dbe9cfa09e6a75f96be62e82615
Kapil Gupta <kpgupta98@gmail.com>
2019-06-18 20:11:29 +0530

    initial codebase

---
+++ experiments/autoencoder/lstm.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import torch
+import torch.nn as nn
+
+class LSTM(nn.module):
+    def __init__(self, config):
+        super(LSTM, self).__init__()
+        self.config = config
